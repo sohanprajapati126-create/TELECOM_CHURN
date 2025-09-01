b5f2db260fc6e00846307fcbacbf9e0db111b6f2
sohanprajapati126-create <sohanprajapati126@gmail.com>
2025-09-01 10:35:40 +0530

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -6,13 +6,14 @@ import joblib
 # Load dataset
 df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-X = df.drop(['customerID', 'Churn'], axis=1)
+# Separate features and target
+X = df.drop(['customerID', 'Churn'], axis=1)   # raw categorical + numeric
 y = df['Churn']
 
 # Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
 
-# Load saved pipeline (includes preprocessing + model)
+# Load saved pipeline (preprocessing + model)
 pipeline = joblib.load('model/Telecome_model.pkl')
 
 # Predict
@@ -21,3 +22,4 @@ y_pred = pipeline.predict(X_test)
 # Accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Model accuracy: {accuracy:.2f}')
+
